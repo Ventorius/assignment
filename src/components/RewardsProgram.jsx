a20f5656a8f59d776f157a8eeeae6037e8f2ee13
Sebastian Basznianin <avaven@gmail.com>
2023-05-13 00:55:24 +0200

    add spinner

--- src/components/RewardsProgram.jsx
+++ src/components/RewardsProgram.jsx
@@ -2,13 +2,19 @@ import { useQuery } from '@tanstack/react-query';
 import { getTransactions } from '../api/transactions.js';
 import { useRewardProgramData } from '../hooks/useRewardProgramData';
 import { UserCard } from './UserCard';
+import { Loader } from './Loader.jsx';
 
 export const RewardsProgram = () => {
   const { data, isLoading } = useQuery({ queryKey: ['transactions'], queryFn: getTransactions });
 
   const rewardProgramData = useRewardProgramData(data);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <Loader />
+      </div>
+    );
 
   return (
     <div className="flex flex-col gap-8 items-center">
